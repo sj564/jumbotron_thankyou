baa7647e283c9ddbce881bb3962f3e5ad422050b
siddharth <sj564@njit.edu>
2018-03-01 17:47:39 -0500

    pagescontroller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,43 +6,46 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-function index()
-{
-return view('pages.index');
-}
+    function index()
+    {
+        return view('pages.index');
+    }
 
-function about()
-{
-return view('pages.about');
-}
+    function about()
+    {
+        return view('pages.about');
+    }
 
-function contact()
-{
-return view('pages.contact');
-}
-function signin()
-{
-return view('pages.signin');
-}
-function signup()
-{
-return view('pages.signup');
-}
-function viewmessage()
-{
-return view('pages.viewmessage');
-}
+    function contact()
+    {
+        return view('pages.contact');
+    }
+
+    function signin()
+    {
+        return view('pages.signin');
+    }
+
+    function signup()
+    {
+        return view('pages.signup');
+    }
+
+    function viewmessage()
+    {
+        return view('pages.viewmessage');
+    }
 
 
-function store(Request $request)
-{
-$name = $request->name;
+    function store(Request $request)
+    {
+        $name = $request->name;
 
-return redirect()->route('thanks', ['name' => $name]);
-}
+        return redirect()->route('thanks', ['name' => $name]);
+    }
 
-function thanks($name, Request $request)
-{
+    function thanks($name, Request $request)
+    {
 
-return view('pages.thankyou')->with(compact('name'));
-}
+        return view('pages.thankyou')->with(compact('name'));
+    }
