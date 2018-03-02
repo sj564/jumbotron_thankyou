39dce74ae54816bb505eeefdd211eeae94a4b99b
siddharth <sj564@njit.edu>
2018-03-01 21:57:37 -0500

    index.php

--- resources/views/pages/viewmessage.blade.php
+++ resources/views/pages/viewmessage.blade.php
@@ -23,11 +23,36 @@
                         <th align="right">Delete</th>
 
                     </tr>
+
                     </thead>
                     <tbody>
                     <tr>
                         <td>1</td>
-                        <td> <a href="http://sid564.herokuapp.com/card">Hello</td>
+                        <td> <a href="http://sj564miniproject.herokuapp.com/card">Hello</td>
+                        <td ><button type="button">Click to delete message</button></td>
+
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td> <a href="http://sj564miniproject.herokuapp.com/card">Hi</td>
+                        <td ><button type="button">Click to delete message</button></td>
+
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td> <a href="http://sj564miniproject.herokuapp.com/card">Hey IS601</td>
+                        <td ><button type="button">Click to delete message</button></td>
+
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td> <a href="http://sj564miniproject.herokuapp.com/card">How are you?</td>
+                        <td ><button type="button">Click to delete message</button></td>
+
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td> <a href="http://sj564miniproject.herokuapp.com/card">Great Weather! Where are you going today</td>
                         <td ><button type="button">Click to delete message</button></td>
 
                     </tr>
@@ -36,3 +61,7 @@
                 </table>
             </div>
         </div>
+
+
+        </tbody>
+        </table>
