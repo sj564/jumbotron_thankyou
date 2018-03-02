292fbd7958eec9d5d3a8c0b077e3faafac2e786b
siddharth <sj564@njit.edu>
2018-03-01 22:05:47 -0500

    about.blade

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,10 +6,11 @@
 
 
 
-    <h1>Hello everyone! This is the about page.</h1>
+    <h1>Hi, this is the about page for the site.</h1>
 
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+    <p class="lead">The WACA (formally the WACA Ground) is a sports stadium in Perth, Western Australia. The stadium's name derives from the initials of its owners and operators, the Western Australian Cricket Association.
 
+        The WACA was Western Australia's "home of cricket" since the early 1890s, with Test cricket played at the ground since the 1970–71 season.[3] The ground is the home venue of Western Australia's first-class cricket team, the Western Warriors, and a Women's National Cricket League side, the Western Fury. The Perth Scorchers, a Big Bash League franchise, also play at the ground, which is branded #TheFurnace for those matches.
 
     </div>
 @endsection
